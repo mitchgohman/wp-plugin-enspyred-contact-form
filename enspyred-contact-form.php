fade53eeefb78f298fc8fec98bc3eae9521fe1a0
Mitch Gohman <mitch@thenounproject.com>
2025-11-24 17:19:11 -0700

    Release v1.0.4

--- enspyred-contact-form.php
+++ enspyred-contact-form.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Enspyred Contact Form
 Description: React + REST contact form with spam controls.
-Version: 1.0.3
+Version: 1.0.4
 Author: Enspyred
 Author URI: https://enspyred.com
 License: GPL v2 or later
